3aa478e4883eb44128e8da676d2b4d539d332da2
yueshutong <yster@foxmail.com>
2019-06-13 18:59:21 +0800

    0.0.1

--- src/main/java/cn/yueshutong/redislock/RedisLock.java
+++ src/main/java/cn/yueshutong/redislock/RedisLock.java
@@ -18,16 +18,26 @@ public class RedisLock {
 
     private static final Long RELEASE_SUCCESS = 1L;
 
-    private long timeout = 5000; //过期时间
+    private long timeout = 5000;
 
     public boolean lock(String key, String value) {
-        //执行set命令
-        Boolean absent = template.opsForValue().setIfAbsent(key, value, timeout, TimeUnit.MILLISECONDS);//1
-        //其实没必要判NULL，这里是为了程序的严谨而加的逻辑
-        if (absent == null) {
-            return false;
+        long start = System.currentTimeMillis();
+        while (true) {
+            //检测是否超时
+            if (System.currentTimeMillis() - start > timeout) {
+                return false;
+            }
+            //执行set命令
+            Boolean absent = template.opsForValue().setIfAbsent(key, value, timeout, TimeUnit.MILLISECONDS);//1
+            //其实没必要判NULL，这里是为了程序的严谨而加的逻辑
+            if (absent == null) {
+                return false;
+            }
+            //是否成功获取锁
+            if (absent) {
+                return true;
+            }
         }
-        return absent;
     }
 
     public boolean unlock(String key, String value) {
